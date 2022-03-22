bbd1921461e6c341bd38c79b9d228676dc892c79
Steven Kelly <stevenkelly418@gmail.com>
2022-03-22 17:39:23 +0000

    => Added  car and carlist html.

--- carsales-nosql/src/app/car/car.component.ts
+++ carsales-nosql/src/app/car/car.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ICar } from '../interfaces/car';
 
 @Component({
   selector: 'app-car',
@@ -7,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CarComponent implements OnInit {
 
+  @Input() carData?:ICar;
+  carImageWidth:number=300;
   constructor() { }
 
   ngOnInit(): void {
